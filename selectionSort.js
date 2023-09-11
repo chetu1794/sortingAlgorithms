2d9659f22601dd6ce68e10c68c97e66186f30d79
chetu1794 <144738753+chetu1794@users.noreply.github.com>
2023-09-11 16:23:31 -0500

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -1,3 +1,8 @@
+/**
+ * Selection sort is a simple and efficient sorting algorithm that works by repeatedly selecting the smallest (or largest) element 
+ * from the unsorted portion of the list and moving it to the sorted portion of the list. 
+ */
+
 let arr=[-Infinity,Infinity,0,0,64,25.8,12,22,-11];
 let len =arr.length;
 
